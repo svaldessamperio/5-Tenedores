38442599fc77307093c425a6f75f811f77aab5df
Samuel Valdes Samperio <svaldessamperio>
2021-03-07 19:47:24 -0600

    Leccion 99

--- screens/Restaurants/AddRestaurant.js
+++ screens/Restaurants/AddRestaurant.js
@@ -1,14 +1,25 @@
-import React from "react";
-import { StyleSheet, View, Text } from "react-native";
+import React, { useState, useRef } from "react";
+import { View } from "react-native";
+import Toast from "react-native-easy-toast";
+import Loading from "../../componentes/Loading";
+import AddRestaurantForm from "../../componentes/Restaurants/AddRestaurantForm";
 
-export default function AddRestaurant() {
+
+export default function AddRestaurant(props) {
+    const { navigation } = props;
+    const [isLoading, setIsLoading] = useState(false);
+    const toastRef = useRef();
+
     return (
         <View>
-            <Text>AddRestaurant.....</Text>
+            <AddRestaurantForm 
+                toastRef={toastRef}
+                setIsLoading={setIsLoading}
+                navigation={navigation}
+            />
+            <Toast ref={toastRef} position="center" opacity={0.9} />
+            <Loading isVisible={isLoading} text="Creando restaurante" />
+
         </View>
     );
 }
-
-const styles = StyleSheet.create({
-
-});
