fbc78a54a2691331a0e78c56afdec34a9d39e27d
Samuel Valdes Samperio <svaldessamperio>
2021-03-07 19:01:35 -0600

    lección 97

--- screens/Restaurants/Restaurants.js
+++ screens/Restaurants/Restaurants.js
@@ -1,11 +1,48 @@
-import React from "react";
-import {View, Text} from "react-native";
+import React, { useState, useEffect } from "react";
+import {StyleSheet, View, Text} from "react-native";
+import { Icon } from "react-native-elements";
+import { firebaseApp } from "../../utils/firebase";
+import firebase from "firebase/app";
+
 
 export default function Restaurants(){
-return(
-  <View>
-      <Text>Restaurants.....</Text>
-  </View>  
-);
 
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    firebase.auth().onAuthStateChanged((userInfo) => {
+      setUser(userInfo);
+    });
+  }, [])
+
+  return(
+    <View style={styles.viewBody}>
+        <Text style={styles.text1}>Restaurants.....</Text>
+        {user && 
+          <Icon
+            type="material-community"
+            name="plus"
+            color="#00a680"
+            reverse
+            containerStyle={styles.btnPlus}
+          /> 
+        }
+        
+    </View>  
+  );
 }
+
+const styles = StyleSheet.create({
+  viewBody: {
+    flex: 1,
+    backgroundColor: "#fff",
+  },
+  btnPlus: {
+    position: "absolute",
+    bottom: 10,
+    right: 10,
+    shadowColor: "black",
+    shadowOffset: {height: 2, width: 2},
+    shadowOpacity: 0.5,
+  },
+});
